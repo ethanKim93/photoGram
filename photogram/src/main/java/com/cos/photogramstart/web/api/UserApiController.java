73f54b2ffe635c1689ac5d8e0406cc32c4568140
ethanKim93 <wsmh.kim12@gmail.com>
2023-02-19 11:49:25 +0900

    구독 모달 정보 API 제작 및 서브쿼리

--- photogram/src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ photogram/src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +19,13 @@ import com.cos.photogramstart.config.auth.PrincipalDetails;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.service.SubscribeService;
 import com.cos.photogramstart.service.UserService;
 import com.cos.photogramstart.web.dto.CMRespDto;
 import com.cos.photogramstart.web.dto.user.UserUpdateDto;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 
+import antlr.collections.List;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -27,7 +33,17 @@ import lombok.RequiredArgsConstructor;
 public class UserApiController {
 
 	private final UserService userService;
-
+	private final SubscribeService subscribeService;
+	
+	@GetMapping("/api/user/{pageUserId}/subscribe") // 페이지 주인이 구독하고 있는 정보 
+	public ResponseEntity<?> subscribeList(@PathVariable int pageUserId, @AuthenticationPrincipal PrincipalDetails principalDetails){
+		
+		List<SubscribeDto> subscribeDto = subscribeService.구독리스트(principalDetails.getUser().getId(), pageUserId);
+		
+		return new ResponseEntity<>(new CMRespDto<>(1, "구독자 정보 리스트 가져오기 성공", subscribeDto), HttpStatus.OK);
+	}
+	
+	
 	// update시 DB 만 바꾸지 말고 세션 정보도 함께 변경해줘야한다
 	@PutMapping("/api/user/{id}")
 	public CMRespDto<?> update(@PathVariable int id, @Valid UserUpdateDto userUpdateDto, BindingResult bindingResult, // 꼭Valid가/적혀있는다음파라메터에적어야함.
